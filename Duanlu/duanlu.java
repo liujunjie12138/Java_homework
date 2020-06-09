4f29007d2588462e77156a7879b311e0d7d33c74
liujunjie12138 <970447020@qq.com>
2020-06-09 13:36:25 +0800

    增加Cal的作业

---
+++ Duanlu/duanlu.java
@@ -0,0 +1,16 @@
+public class Duanlu{
+	public static void main (String [] args){
+		int a,b,c;
+		a=b=0;
+		c=5;
+		System.out.println(a>c||b>c++);
+		System.out.println(c);
+
+	
+		System.out.println(a>c|b>c++);
+		System.out.println(c);
+
+
+	}
+  
+} 
