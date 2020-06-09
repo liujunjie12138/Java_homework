77c81cea7192e4a1b1e5da0979da3645aac36910
liujunjie12138 <970447020@qq.com>
2020-06-09 13:50:47 +0800

    增加qiangz作业

---
+++ qiangzhi.java
@@ -0,0 +1,12 @@
+public class Qiangzhi{
+	public static void main (String[] args){
+		float a=3.141592697E+30F;
+		int b;
+		
+		b=(int)a;
+
+		System.out.println(a);
+
+		System.out.println(b);
+	}
+}
